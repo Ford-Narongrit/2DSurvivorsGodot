96a1153fcffc40416df835f009b5376da47731b5
ford-narongrit <narongrit@playtorium.co.th>
2024-03-01 15:01:39 +0700

    add pause menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -6,6 +6,7 @@ func _ready():
 	%PlayButton.pressed.connect(on_play_pressed)
 	%OptionsButton.pressed.connect(on_options_pressed)
 	%QuitButton.pressed.connect(on_quit_pressed)
+	
 
 
 func on_play_pressed():
---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,66 @@
+extends CanvasLayer
+
+@onready var panel_container = %PanelContainer
+@onready var resume_button = %ResumeButton
+@onready var options_button = %OptionsButton
+@onready var quit_button = %QuitButton
+
+var is_closing: bool = false
+var options_menu_scene =preload("res://scenes/ui/options_menu.tscn")
+
+func _ready():
+	get_tree().paused = true
+	
+	resume_button.pressed.connect(on_resume_pressed)
+	options_button.pressed.connect(on_options_pressed)
+	quit_button.pressed.connect(on_quit_pressed)
+	
+	panel_container.pivot_offset = panel_container.size / 2
+	$AnimationPlayer.play("in")
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		get_tree().root.set_input_as_handled()
+		close()
+
+
+func close():
+	if is_closing:
+		return
+	
+	is_closing = true
+	$AnimationPlayer.play("out")
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0.3)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	
+	await  tween.finished
+	get_tree().paused = false
+	queue_free()
+
+
+func on_resume_pressed():
+	close()
+
+
+func on_options_pressed():
+	var options_menu_instance = options_menu_scene.instantiate()
+	add_child(options_menu_instance)
+	options_menu_instance.back_pressed.connect(on_options_back_pressed.bind(options_menu_instance))
+
+
+func on_quit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+
+func on_options_back_pressed(options_instance: Node):
+	options_instance.queue_free()
