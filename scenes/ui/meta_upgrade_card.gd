ccaf4600e3fb26b7a362244493d6b1f495469eff
ford-narongrit <narongrit@playtorium.co.th>
2024-03-02 10:15:49 +0700

    add scrollContaniner to meta scene

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -36,6 +36,7 @@ func update_progress():
 	percent = min(percent,1)
 	progress_bar.value = percent
 	purchase_button.disabled = percent < 1 || is_maxed
+	purchase_button.disabled = percent < 1 || is_maxed
 	
 	if is_maxed:
 		purchase_button.text = "Max"
